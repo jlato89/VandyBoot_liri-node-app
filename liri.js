f029de0af41fdc39be45f27bc0be0c7c53adfdd6
Josh Latour <jlatour88@gmail.com>
2019-06-27 23:36:18 -0500

    added axios call. not pulling anything but response

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var axios = require("axios");
 // var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 // var userParm = process.argv.splice(2, process.argv.length - 1); //! needs .trim() ???
@@ -8,22 +9,22 @@ var value = process.argv[3];
 switch(action) {
    case "concert-this":
       console.log('Command -> Concert This');
-      concertThis();
+      concertThis(value);
       break;
 
    case "spotify-this-song":
       console.log('Command -> Spotify This Song');
-      spotifyThisSong();
+      spotifyThisSong(value);
       break;
 
    case "movie-this":
       console.log('Command -> Movie This');
-      movieThis();
+      movieThis(value);
       break;
 
    case "do-what-it-says":
       console.log('Command -> Do What It Says');
-      doWhatItSays();
+      doWhatItSays(value);
       break;
       
    default:
@@ -33,18 +34,38 @@ switch(action) {
 
 
 //*   FUNCTIONS
-function concertThis() {
+function concertThis(value) {
+   var artist = value;
+   console.log('value is '+artist);
+
+   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+      function (response) {
+         console.log(response);
+         // console.log(response.venue.city+", "+response.venue.region);
+      })
+      .catch(function (error) {
+         if (error.response) {
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+         } else if (error.request) {console.log(error.request);
+         } else {console.log("Error", error.message);}
+         console.log(error.config);
+      });
 
 }
 
-function spotifyThisSong() {
+function spotifyThisSong(value) {
 
 }
 
-function movieThis() {
+function movieThis(value) {
 
 }
 
-function doWhatItSays() {
+function doWhatItSays(value) {
 
 }
